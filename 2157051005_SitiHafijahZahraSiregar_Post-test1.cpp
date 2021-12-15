edb916ad4ee9ad42359602ec66fe9b24049b3d4d
SitiHafijahZahraSiregar <92381404+SitiHafijahZahraSiregar@users.noreply.github.com>
2021-12-15 21:44:47 +0700

    Add files via upload

---
+++ 2157051005_SitiHafijahZahraSiregar_Post-test1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	/* Nama : Siti Hafijah Zahra Siregar
+	   NPM  : 2157051005
+	   Kelas: B' 2021
+	*/ 
+	
+	//string//
+	string npm , tanggal_lahir , bulan_lahir , tahun_lahir ;
+	
+	//input//
+	
+	cout << "Masukkan npm : "; cin >> npm;
+	cout << "Masukkan tanggal_lahir : "; cin >> tanggal_lahir;
+	cout << "Masukkan Bulan_lahir : "; cin >> bulan_lahir;
+	cout << "Masukkan Tahun_lahir : "; cin >> tahun_lahir;
+	
+	cout << endl << endl;
+	
+	cout << "Username : " << npm << endl;
+	cout << "Password : " << tahun_lahir  << bulan_lahir <<  tanggal_lahir << endl;
+	
+	return 0;
+}
